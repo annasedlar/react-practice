3ea50286893a8bf8a9a98dba4a5407df49ae2ce4
Anna Sedlar <annasedlar@gmail.com>
2017-01-10 10:41:10 -0500

    first reactDOM render

---
+++ react-form.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+var Form = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<h1>IT WORKS!</h1>
+			</div>
+		)
+	}
+});
+
+ReactDOM.render(
+	<Form />, 
+	document.getElementById('form')
+)
+
+
+
+
