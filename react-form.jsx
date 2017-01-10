f565b15953e2bf56a52440ce15e4d6f9a5394687
Anna Sedlar <annasedlar@gmail.com>
2017-01-10 10:59:50 -0500

    add form field

--- react-form.jsx
+++ react-form.jsx
@@ -3,10 +3,26 @@
 
 
 var Form = React.createClass({
+	getInitialState: function() {
+		return{
+			value: '2017'
+		}
+	},
+
+	handleChange: function(event){
+		this.setState({
+			value: event.target.value
+		});
+	},
+
 	render: function(){
 		return(
 			<div>
-				<h1>IT WORKS!</h1>
+				<h1>How old are you?</h1>
+				<form>
+					<input type="text" textToDisplay="Type birthyear" />
+					<input type="submit" value="click me!" onClick={this.handleChange} />
+				</form>
 			</div>
 		)
 	}
