6b0ff06aed645225bb16fff4edba1658199a3ad0
Anna Sedlar <annasedlar@gmail.com>
2017-01-10 11:30:11 -0500

    more form functions

--- react-form.jsx
+++ react-form.jsx
@@ -1,11 +1,24 @@
+function Calculate(userYear){
+	var d = new Date(); 
+	var n = d.getFullYear();
+	var userAge = (n - userYear); 
+	return(userAge); 
+}
 
-
-
+function Year(props){
+	return(
+		<div>
+			<p> Birthyear is {props.textToDisplay}. Therefore.... 
+			</p>
+			<p>You are {props.value} years old!</p>
+		</div>	
+		)
+}
 
 var Form = React.createClass({
 	getInitialState: function() {
 		return{
-			value: '2017'
+			value: ''
 		}
 	},
 
@@ -16,12 +29,14 @@ var Form = React.createClass({
 	},
 
 	render: function(){
+		var age = Calculate()
 		return(
 			<div>
-				<h1>How old are you?</h1>
+				<h1>Let us find out how old you are!</h1>
 				<form>
-					<input type="text" textToDisplay="Type birthyear" />
+					<input type="text" defaultValue="Type birthyear" />
 					<input type="submit" value="click me!" onClick={this.handleChange} />
+					<Year textToDisplay={this.state.value} value={this.age}/>
 				</form>
 			</div>
 		)
